78471e80b674f83675eab0fe75bd0fab07b0f1d0
Jun Anthony Calupcupan <junanthony.business@gmail.com>
2024-03-10 10:54:41 +1300

    Add Issue Model

---
+++ AntoniosTalyer/Models/Issue.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AntoniosTalyer.Models
+{
+    public class Issue
+    {
+        [Key]
+        public int IssueId { get; set; }
+        public string Problem { get; set; }
+        public string PartsNeed { get; set; }
+        public string PartsPrice { get; set; }
+        public string Description { get; set; }
+    }
+}
