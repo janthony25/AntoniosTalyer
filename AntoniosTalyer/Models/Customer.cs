08dc4ac1f31d7cac36f68ffadc90a3489975d292
Jun Anthony Calupcupan <junanthony.business@gmail.com>
2024-03-10 10:52:31 +1300

    Add Customer model

---
+++ AntoniosTalyer/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AntoniosTalyer.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CarMake { get; set; }
+        public string CarModel { get; set; }
+        public string CarRego { get; set; }
+    }
+}
