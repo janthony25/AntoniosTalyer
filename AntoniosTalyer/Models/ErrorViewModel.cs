553f7ec5e86dcd73289da884648faedac31bd3af
Jun Anthony Calupcupan <junanthony.business@gmail.com>
2024-03-10 10:48:30 +1300

    Add project files.

---
+++ AntoniosTalyer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AntoniosTalyer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
