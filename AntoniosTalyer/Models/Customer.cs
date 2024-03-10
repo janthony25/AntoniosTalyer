3208b47a720e0da12fe8595975a0afc2f36683f2
Jun Anthony Calupcupan <junanthony.business@gmail.com>
2024-03-10 19:18:11 +1300

    Finish Customer and Issue model

--- AntoniosTalyer/Models/Customer.cs
+++ AntoniosTalyer/Models/Customer.cs
@@ -6,10 +6,17 @@ namespace AntoniosTalyer.Models
     {
         [Key]
         public int CustomerId { get; set; }
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public string CarMake { get; set; }
+        [Required]
         public string CarModel { get; set; }
+        [Required]
         public string CarRego { get; set; }
+
+        public ICollection<Issue> Issues { get; set; }
     }
 }
--- AntoniosTalyer/Models/Issue.cs
+++ AntoniosTalyer/Models/Issue.cs
@@ -6,9 +6,16 @@ namespace AntoniosTalyer.Models
     {
         [Key]
         public int IssueId { get; set; }
+        [Required]
         public string Problem { get; set; }
-        public string PartsNeed { get; set; }
-        public string PartsPrice { get; set; }
-        public string Description { get; set; }
+        public string? PartsNeed { get; set; }
+        public Decimal? PartsPrice { get; set; }
+        public string? Description { get; set; }
+        public decimal? TotalPrice { get; set; }
+        public decimal? LaborPrice { get; set; }
+
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+
     }
 }
