3deabe6cf2185ce6762dd63c4ec9b497ee212c7b
Jun Anthony Calupcupan <junanthony.business@gmail.com>
2024-03-10 19:26:03 +1300

    Add Fluent API to connect Issue to CustomerId

---
+++ AntoniosTalyer/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using AntoniosTalyer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AntoniosTalyer.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Issue> Issues { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Issue>()
+                .HasOne(issue => issue.Customer)
+                .WithMany(customer => customer.Issues)
+                .HasForeignKey(issue => issue.CustomerId)
+                .IsRequired();
+        }
+    }
+}
